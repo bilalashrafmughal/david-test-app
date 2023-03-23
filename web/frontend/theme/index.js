59d839aa5c7f6bb52afb7e03344e150b9259e3fc
bilalashrafCodeFulcrum <bilal.ashraf@codefulcrum.com>
2023-03-23 21:29:19 +0500

    2nd commit

--- web/frontend/theme/index.js
+++
@@ -1,9 +0,0 @@
-import { themeConfig } from "./themeConfig";
-
-export const getTheme = (themeName = "light") => {
-  const requiredTheme = themeConfig[themeName];
-  if (!requiredTheme) {
-    return themeConfig["light"];
-  }
-  return requiredTheme;
-};
